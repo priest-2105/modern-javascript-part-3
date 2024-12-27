a8152529ffb1f7c82fa21514575344d671e5dcc2
priest-2105 <priest2105@gmail.com>
2024-12-28 00:27:37 +0100

    commit

--- script/11-todo-list.js
+++ script/11-todo-list.js
@@ -7,19 +7,16 @@ const todoParagraph_El = document.querySelector('.todo-paragraph');
 
 function addTodoList(){
 
+      todoArr.push(todoInput_El.value);
 
-      for (let i = 0; i < todoArr.length; i++) {
-        todoArr.push(todoInput_El.value);
-    
-        const paragraph =  document.createElement('p');
-          paragraph.innerHTML = todoArr[i];
-          todoParagraph_El.appendChild(paragraph);
-        
-          console.log(todoArr[i]);
-     
-        } 
-       
+
+
+for (let i = 0; i < todoArr.length; i++) {
+   
 }
+}
+
+
 
 todoButton_El.addEventListener('click', () => {
 
